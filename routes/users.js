78d4784994bdfbebf8b1c442b34cd507a664b5bc
Caleb Matteis <matteismusic@gmail.com>
2021-01-29 07:59:56 -0500

    added test for users route

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@
 const jsonschema = require("jsonschema");
 
 const express = require("express");
-const { ensureCorrectUserorAdmin, ensureAdmin } = require("../middleware/auth");
+const { ensureCorrectUserOrAdmin, ensureAdmin } = require("../middleware/auth");
 const { BadRequestError } = require("../expressError");
 const User = require("../models/user");
 const { createToken } = require("../helpers/tokens");
@@ -24,7 +24,7 @@ const router = express.Router();
  * This returns the newly created user and an authentication token for them:
  *  {user: { username, firstName, lastName, email, isAdmin }, token }
  *
- * Authorization required: login
+ * Authorization required: admin
  **/
 
 router.post("/", ensureAdmin, async function (req, res, next) {
@@ -48,7 +48,7 @@ router.post("/", ensureAdmin, async function (req, res, next) {
  *
  * Returns list of all users.
  *
- * Authorization required: login
+ * Authorization required: admin
  **/
 
 router.get("/", ensureAdmin, async function (req, res, next) {
@@ -63,12 +63,13 @@ router.get("/", ensureAdmin, async function (req, res, next) {
 
 /** GET /[username] => { user }
  *
- * Returns { username, firstName, lastName, isAdmin }
+ * Returns { username, firstName, lastName, isAdmin, jobs }
+ *   where jobs is { id, title, companyHandle, companyName, state }
  *
- * Authorization required: login
+ * Authorization required: admin or same user-as-:username
  **/
 
-router.get("/:username", ensureAdmin, async function (req, res, next) {
+router.get("/:username", ensureCorrectUserOrAdmin, async function (req, res, next) {
   try {
     const user = await User.get(req.params.username);
     return res.json({ user });
@@ -85,10 +86,10 @@ router.get("/:username", ensureAdmin, async function (req, res, next) {
  *
  * Returns { username, firstName, lastName, email, isAdmin }
  *
- * Authorization required: admin or same user as :username
+ * Authorization required: admin or same-user-as-:username
  **/
 
-router.patch("/:username", ensureCorrectUserorAdmin, async function (req, res, next) {
+router.patch("/:username", ensureCorrectUserOrAdmin, async function (req, res, next) {
   try {
     const validator = jsonschema.validate(req.body, userUpdateSchema);
     if (!validator.valid) {
@@ -106,10 +107,10 @@ router.patch("/:username", ensureCorrectUserorAdmin, async function (req, res, n
 
 /** DELETE /[username]  =>  { deleted: username }
  *
- * Authorization required: admin or same user as :username
+ * Authorization required: admin or same-user-as-:username
  **/
 
-router.delete("/:username", ensureAdmin, async function (req, res, next) {
+router.delete("/:username", ensureCorrectUserOrAdmin, async function (req, res, next) {
   try {
     await User.remove(req.params.username);
     return res.json({ deleted: req.params.username });
@@ -118,19 +119,19 @@ router.delete("/:username", ensureAdmin, async function (req, res, next) {
   }
 });
 
-/* POST /[usernam]/jobs[id] { state } => { application } 
 
-Returns {"applied": jobId}
+/** POST /[username]/jobs/[id]  { state } => { application }
+ *
+ * Returns {"applied": jobId}
+ *
+ * Authorization required: admin or same-user-as-:username
+ * */
 
-Authorization required: admin or same user as :username
-
-*/
-
-router.post("/username/jobs/:id", ensureCorrectUserorAdmin, async function (req, res, nest) {
+router.post("/:username/jobs/:id", ensureCorrectUserOrAdmin, async function (req, res, next) {
   try {
     const jobId = +req.params.id;
     await User.applyToJob(req.params.username, jobId);
-    return res.join({ applied: jobId });
+    return res.json({ applied: jobId });
   } catch (err) {
     return next(err);
   }
